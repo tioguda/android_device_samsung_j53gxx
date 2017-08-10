29e48fc27485e6bf5b3aec39c62b07dba75b7d45
tioguda <guda.flavio@gmail.com>
2017-08-09 21:29:03 -0300

    BringUp Tesla

--- tesla.mk
+++ tesla.mk
@@ -1,12 +1,11 @@
-
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/lineage.mk)
+$(call inherit-product, device/samsung/j5-common/tesla.mk)
 
 $(call inherit-product, device/samsung/j53gxx/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j53gxx
-PRODUCT_NAME := lineage_j53gxx
+PRODUCT_NAME := tesla_j53gxx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J500H
 PRODUCT_MANUFACTURER := samsung
