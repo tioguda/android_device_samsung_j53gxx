d0259b2478c3c6e7bf225918518405da8d678cc6
tioguda <guda.flavio@gmail.com>
2017-10-15 07:59:13 -0200

    Remove support for OTAUpdates

--- device.mk
+++ device.mk
@@ -1,8 +1,3 @@
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.ota.romname=Tesla-j53gxx \
-    ro.ota.version=$(shell date +%F%H | sed s@-@@g) \
-    ro.ota.manifest=https://basketbuild.com/uploads/devs/tioguda/Tesla/j53gxx/7.1.2/j53gxx.xml
-
 #Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/j53gxx/j53gxx-vendor.mk)
 
